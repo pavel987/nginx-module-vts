687eda37ccba562a44ad63caf88fc4e9d3d91a3e
Pavel Shub <pavel.987@gmail.com>
2018-02-11 23:03:13 -0500

    added server request count metrics, few more to go

--- src/ngx_http_vhost_traffic_status_prom_display.c
+++ src/ngx_http_vhost_traffic_status_prom_display.c
@@ -38,19 +38,101 @@ ngx_http_vhost_traffic_status_prom_display_set_main(ngx_http_request_t *r,
 }
 
 u_char *
+ngx_http_vhost_traffic_status_prom_display_set_server(ngx_http_request_t *r,
+                                                 u_char *buf, ngx_rbtree_node_t *node)
+{
+    ngx_str_t                                  key;
+    ngx_http_vhost_traffic_status_ctx_t       *ctx;
+    ngx_http_vhost_traffic_status_node_t      *vtsn;
+    ngx_http_vhost_traffic_status_loc_conf_t  *vtscf;
+
+    ctx = ngx_http_get_module_main_conf(r, ngx_http_vhost_traffic_status_module);
+
+    vtscf = ngx_http_get_module_loc_conf(r, ngx_http_vhost_traffic_status_module);
+
+    if (node != ctx->rbtree->sentinel) {
+        vtsn = (ngx_http_vhost_traffic_status_node_t *) &node->color;
+
+        if (vtsn->stat_upstream.type == NGX_HTTP_VHOST_TRAFFIC_STATUS_UPSTREAM_NO) {
+            key.data = vtsn->data;
+            key.len = vtsn->len;
+
+            buf = ngx_http_vhost_traffic_status_prom_display_set_server_node(r, buf, &key, vtsn);
+
+            /* calculates the sum */
+            vtscf->stats.stat_request_counter +=vtsn->stat_request_counter;
+            vtscf->stats.stat_in_bytes += vtsn->stat_in_bytes;
+            vtscf->stats.stat_out_bytes += vtsn->stat_out_bytes;
+            vtscf->stats.stat_1xx_counter += vtsn->stat_1xx_counter;
+            vtscf->stats.stat_2xx_counter += vtsn->stat_2xx_counter;
+            vtscf->stats.stat_3xx_counter += vtsn->stat_3xx_counter;
+            vtscf->stats.stat_4xx_counter += vtsn->stat_4xx_counter;
+            vtscf->stats.stat_5xx_counter += vtsn->stat_5xx_counter;
+            ngx_http_vhost_traffic_status_node_time_queue_merge(
+                    &vtscf->stats.stat_request_times,
+                    &vtsn->stat_request_times, vtscf->average_period);
+
+        }
+
+        buf = ngx_http_vhost_traffic_status_prom_display_set_server(r, buf, node->left);
+        buf = ngx_http_vhost_traffic_status_prom_display_set_server(r, buf, node->right);
+    }
+
+    return buf;
+}
+
+u_char *
+ngx_http_vhost_traffic_status_prom_display_set_server_node(
+        ngx_http_request_t *r,
+        u_char *buf, ngx_str_t *key,
+        ngx_http_vhost_traffic_status_node_t *vtsn)
+{
+    ngx_int_t                                  rc;
+    ngx_str_t                                  tmp, dst;
+    ngx_http_vhost_traffic_status_loc_conf_t  *vtscf;
+
+    vtscf = ngx_http_get_module_loc_conf(r, ngx_http_vhost_traffic_status_module);
+
+    tmp = *key;
+
+    (void) ngx_http_vhost_traffic_status_node_position_key(&tmp, 1);
+
+    rc = ngx_http_vhost_traffic_status_escape_json_pool(r->pool, &dst, &tmp);
+    if (rc != NGX_OK) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                      "display_set_server_node::escape_json_pool() failed");
+    }
+
+    buf = ngx_sprintf(buf, NGX_HTTP_VHOST_TRAFFIC_STATUS_PROM_FMT_SERVER,
+                      &dst, vtsn->stat_1xx_counter,
+                      &dst, vtsn->stat_2xx_counter,
+                      &dst, vtsn->stat_3xx_counter,
+                      &dst, vtsn->stat_4xx_counter,
+                      &dst, vtsn->stat_5xx_counter,
+                      &dst, vtsn->stat_request_counter,
+                      &dst, vtsn->stat_in_bytes,
+                      &dst, vtsn->stat_out_bytes,
+                      &dst, ngx_http_vhost_traffic_status_node_time_queue_average(
+                              &vtsn->stat_request_times, vtscf->average_method,
+                              vtscf->average_period));
+
+    return buf;
+}
+
+u_char *
 ngx_http_vhost_traffic_status_prom_display_set(ngx_http_request_t *r,
                                           u_char *buf)
 {
 //    u_char                                    *o, *s;
-//    ngx_rbtree_node_t                         *node;
-//    ngx_http_vhost_traffic_status_ctx_t       *ctx;
+    ngx_rbtree_node_t                         *node;
+    ngx_http_vhost_traffic_status_ctx_t       *ctx;
     ngx_http_vhost_traffic_status_loc_conf_t  *vtscf;
 
-//    ctx = ngx_http_get_module_main_conf(r, ngx_http_vhost_traffic_status_module);
+    ctx = ngx_http_get_module_main_conf(r, ngx_http_vhost_traffic_status_module);
 
     vtscf = ngx_http_get_module_loc_conf(r, ngx_http_vhost_traffic_status_module);
 
-//    node = ctx->rbtree->root;
+    node = ctx->rbtree->root;
 
     /* init stats */
     ngx_memzero(&vtscf->stats, sizeof(vtscf->stats));
@@ -59,19 +141,18 @@ ngx_http_vhost_traffic_status_prom_display_set(ngx_http_request_t *r,
     /* main & connections */
 
     buf = ngx_http_vhost_traffic_status_prom_display_set_main(r, buf);
+
+    /* serverZones */
+
+    buf = ngx_http_vhost_traffic_status_prom_display_set_server(r, buf, node);
+
+    buf = ngx_http_vhost_traffic_status_prom_display_set_server_node(r, buf, &vtscf->sum_key,
+                                                                &vtscf->stats);
 //
-//    /* serverZones */
-//    buf = ngx_sprintf(buf, NGX_HTTP_VHOST_TRAFFIC_STATUS_JSON_FMT_SERVER_S);
-//
-//    buf = ngx_http_vhost_traffic_status_display_set_server(r, buf, node);
-//
-//    buf = ngx_http_vhost_traffic_status_display_set_server_node(r, buf, &vtscf->sum_key,
-//                                                                &vtscf->stats);
-//
-//    buf--;
+//    buf--; // FIXME wtf is this for?
 //    buf = ngx_sprintf(buf, NGX_HTTP_VHOST_TRAFFIC_STATUS_JSON_FMT_E);
 //    buf = ngx_sprintf(buf, NGX_HTTP_VHOST_TRAFFIC_STATUS_JSON_FMT_NEXT);
-//
+
 //    /* filterZones */
 //    o = buf;
 //
--- src/ngx_http_vhost_traffic_status_prom_display.h
+++ src/ngx_http_vhost_traffic_status_prom_display.h
@@ -14,8 +14,45 @@
     "nginx_server_connections{status=\"waiting\"} %uA\n" \
     "nginx_server_connections{status=\"writing\"} %uA\n"
 
+#define NGX_HTTP_VHOST_TRAFFIC_STATUS_PROM_FMT_SERVER \
+    "# HELP nginx_server_requests requests counter\n" \
+    "# TYPE nginx_server_requests counter\n" \
+    "nginx_server_requests{code=\"1xx\",host=\"%V\"} %uA\n" \
+    "nginx_server_requests{code=\"2xx\",host=\"%V\"} %uA\n" \
+    "nginx_server_requests{code=\"3xx\",host=\"%V\"} %uA\n" \
+    "nginx_server_requests{code=\"4xx\",host=\"%V\"} %uA\n" \
+    "nginx_server_requests{code=\"5xx\",host=\"%V\"} %uA\n" \
+    "nginx_server_requests{code=\"total\",host=\"%V\"} %uA\n" \
+    "# HELP nginx_server_bytes request/response bytes\n" \
+    "# TYPE nginx_server_bytes counter\n" \
+    "nginx_server_bytes{direction=\"in\",host=\"%V\"} %uA\n" \
+    "nginx_server_bytes{direction=\"out\",host=\"%V\"} %uA\n" \
+    "# HELP nginx_server_requestMsec average of request processing times in milliseconds\n" \
+    "# TYPE nginx_server_requestMsec gauge\n" \
+    "nginx_server_requestMsec{host=\"%V\"} %M\n"
+
+#define NGX_HTTP_VHOST_TRAFFIC_STATUS_PROM_FMT_SERVER_CACHE \
+    "# HELP nginx_cache_requests cache requests counter\n" \
+    "# TYPE nginx_cache_requests counter\n" \
+    "\"miss\":%uA,"                                                            \
+    "\"bypass\":%uA,"                                                          \
+    "\"expired\":%uA,"                                                         \
+    "\"stale\":%uA,"                                                           \
+    "\"updating\":%uA,"                                                        \
+    "\"revalidated\":%uA,"                                                     \
+    "\"hit\":%uA,"                                                             \
+    "\"scarce\":%uA"
+
+
 u_char *ngx_http_vhost_traffic_status_prom_display_set(ngx_http_request_t *r,
                                                   u_char *buf);
+u_char *ngx_http_vhost_traffic_status_prom_display_set_server(
+        ngx_http_request_t *r, u_char *buf,
+        ngx_rbtree_node_t *node);
+u_char *ngx_http_vhost_traffic_status_prom_display_set_server_node(
+        ngx_http_request_t *r,
+        u_char *buf, ngx_str_t *key,
+        ngx_http_vhost_traffic_status_node_t *vtsn);
 
 u_char *ngx_http_vhost_traffic_status_prom_display_set_main(
         ngx_http_request_t *r, u_char *buf);
